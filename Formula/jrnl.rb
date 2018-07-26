e5d32ae631253695a7bfe8f7ebe4124ce293165c
cjsaylor <cjsaylor@gmail.com>
2018-07-25 21:25:12 -0400

    jrnl version v0.4.0

--- Formula/jrnl.rb
+++ Formula/jrnl.rb
@@ -1,9 +1,9 @@
 class Jrnl < Formula
   desc "Quick and easy CLI journaling tool for Github wiki journals."
   homepage ""
-  url "https://github.com/cjsaylor/jrnl/releases/download/v0.3.0/jrnl_0.3.0_darwin_amd64.tar.gz"
-  version "0.3.0"
-  sha256 "203eef785b9e7bf66814b029bab9d1c583803157ddc2b494a68d399555309c28"
+  url "https://github.com/cjsaylor/jrnl/releases/download/v0.4.0/jrnl_0.4.0_darwin_amd64.tar.gz"
+  version "0.4.0"
+  sha256 "0107b2159f28c089fe1a1ee73a5a65aa6c2dd576b4e9e60b4a984355a4749a27"
   
   depends_on "git"
 
