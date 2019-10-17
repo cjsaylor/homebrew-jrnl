d3c37de0d5828a99f2eb5fe279dba810613f2b66
cjsaylor <cjsaylor@gmail.com>
2019-10-16 20:53:33 -0400

    Brew formula update for jrnl version v0.6.0

--- Formula/jrnl.rb
+++ Formula/jrnl.rb
@@ -1,17 +1,13 @@
 class Jrnl < Formula
   desc "Quick and easy CLI journaling tool for Github wiki journals."
   homepage ""
-  url "https://github.com/cjsaylor/jrnl/releases/download/v0.5.0/jrnl_0.5.0_darwin_amd64.tar.gz"
-  version "0.5.0"
-  sha256 "06f3a88a712aaed8f59cd97f8665f5154e4297f47f16ae32099b9f65d0ea27cc"
+  url "https://github.com/cjsaylor/jrnl/releases/download/v0.6.0/jrnl_0.6.0_darwin_amd64.tar.gz"
+  version "0.6.0"
+  sha256 "d56bf42c3644243b3dd894d29196d3d0d8d50a5681f64335f9940c7ca49fadf5"
   
   depends_on "git"
 
   def install
     bin.install "jrnl"
-  end
-
-  test do
-    
   end
 end
